40e1d34c085d11d1d6144141c6ae59873d0fa747
Shivansh Mahajan <shivanshmmahajan@gmail.com>
2025-12-31 22:35:46 +0530

    DbSetup

---
+++ backend/ShoppingPortal.API/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingPortal.APi.Data;
+using ShoppingPortal.APi.DTOs;
+using ShoppingPortal.APi.Models;
+
+namespace ShoppingPortal.APi.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController: ControllerBase
+{
+    private ShoppingPortalDbContext dc;
+
+    public AuthController(ShoppingPortalDbContext context)
+    {
+        dc = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterRequestDTO dto)
+    {
+        var userExists = dc.Users.Any(u => u.Email == dto.Email);
+        if (userExists)
+        {
+            return BadRequest("User with this email already exists.");
+        }
+
+        var user = new User
+        {
+            Name = dto.Name,
+            Email = dto.Email,
+            Password = dto.Password,
+            RoleId = 2 // Default role as Customer
+        };
+
+        dc.Users.Add(user);
+        await dc.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(Register), user);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginRequestDTO dto)
+    {
+        var user = await dc.Users.FirstOrDefaultAsync(u=> u.Email == dto.Email && u.Password == dto.Password);
+        if (user == null)
+        {
+            return Unauthorized("Invalid email or password.");
+        }
+
+        return Ok(new LoginResponseDTO
+        {
+            Message = "Login successful",
+            Token = "dummy",
+            Role = user.Role.RoleName
+        });
+    }
+
+}
--- backend/ShoppingPortal.API/Data/ShoppingPortalDbContext.cs
+++ backend/ShoppingPortal.API/Data/ShoppingPortalDbContext.cs
@@ -1,8 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using ShoppingPortal.APi.Models;
 
 namespace ShoppingPortal.APi.Data;
 
 public class ShoppingPortalDbContext: DbContext
 {
     public ShoppingPortalDbContext(DbContextOptions<ShoppingPortalDbContext> options) : base(options) {}
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
 }
